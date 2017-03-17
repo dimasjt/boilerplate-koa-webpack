c8337bc60223ccb7f421304253ce1e195681cb63
Dimas J. Taniawan <dimazniawan@gmail.com>
2017-03-17 22:48:55 +0700

    fist initialize

---
+++ server.js
@@ -0,0 +1,20 @@
+const Koa = require('koa');
+const logger = require('koa-logger');
+const Pug = require('koa-pug');
+
+const app = new Koa();
+const routes = require('./config/routes.js');
+
+const pug = new Pug({
+  viewPath: './app/views',
+  debug: true,
+  pretty: true,
+  compileDebug: false,
+  app: app
+})
+
+app.use(logger());
+
+routes(app);
+
+app.listen(3000)
