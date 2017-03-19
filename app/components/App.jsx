8b349f7a579905d8c16c32aaea128ed1bd7ba5d4
Dimas J. Taniawan <dimazniawan@gmail.com>
2017-03-19 22:21:27 +0700

    react-router

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import routes from './routes.jsx';
 
 render((
-  <Router history={browserHistory}>
+  <Router>
     { routes }
   </Router>
 ), document.getElementById('root'))
--- app/components/routes.jsx
+++ app/components/routes.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route, Link } from 'react-router';
+import { Route, Link } from 'react-router-dom';
 
 import Main from './layouts/Main.jsx';
 import Home from './pages/Home.jsx';
 
 module.exports = (
-  <Route path="/" component={Main}>
+  <Route exact={true} path="/" component={Main}>
     <Route path="/home" component={Home} />
   </Route>
 );
